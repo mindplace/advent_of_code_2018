009cf0e03a1ad478e2d5eb1422b8d42e457bc16b
Esther Leytush <esther.leytush@betterment.com>
2018-12-06 21:15:44 -0500

    arrive at naive initial solution for day 1, part 2

--- day_1_chronal_calibration/chronal_calibrator.rb
+++ day_1_chronal_calibration/chronal_calibrator.rb
@@ -1,37 +1,42 @@
 class ChronalCalibrator
-  attr_reader :values, :frequency_changes
+  attr_reader :values, :frequency_changes, :total_resulting_frequency, :nth_recurring, :frequency_occurance_graph
 
   def initialize(values:)
     @values = values
-    @frequency_changes = []
+    @frequency_changes = [0]
+    @total_resulting_frequency = 0
+    @nth_recurring = nil
   end
 
   def calculate_resulting_frequency
     values.reduce(&:+)
   end
 
-  def calculate_frequency_changes
-    until frequency_changes.uniq.length != frequency_changes.length
-      current_total = 0
-      values.each_with_index do |value, i|
-        @frequency_changes.push(current_total += 1)
-      end
+  def nth_recurring_frequency(n:)
+    while nth_recurring.nil?
+      generate_frequency_changes
+      generate_frequencies_hash(n)
     end
-    frequency_changes
+    nth_recurring
   end
 
-  def nth_recurring_frequency(n)
-    frequency_occurance_graph = Hash.new(0)
-    nth_recurring = nil
+  private
 
+  def generate_frequency_changes
+    current_total = total_resulting_frequency
+    values.each { |value| @frequency_changes.push(current_total += value) }
+    @total_resulting_frequency = current_total
+  end
+
+  def generate_frequencies_hash(n)
+    frequency_graph = Hash.new(0)
     frequency_changes.each do |value|
-      frequency_occurance_graph[value] += 1
-      if frequency_occurance_graph[value] == n
-        nth_recurring = value
-        break
+      frequency_graph[value] += 1
+      if frequency_graph[value] == n
+        @nth_recurring = value
+        @frequency_occurance_graph = frequency_graph
+        return value
       end
     end
-
-    nth_recurring
   end
 end
--- day_1_chronal_calibration/chronal_calibrator_spec.rb
+++ day_1_chronal_calibration/chronal_calibrator_spec.rb
@@ -1,46 +1,92 @@
 require 'rspec'
+require 'pry'
 require_relative 'chronal_calibrator.rb'
 
 RSpec.describe ChronalCalibrator do
-  let(:chronal_calibrator) { ChronalCalibrator.new(values: values) }
+  let(:subject) { ChronalCalibrator.new(values: values) }
+  let(:file_values) { File.readlines('input_values.txt').map { |line| line.chomp.to_i } }
 
   describe '#calculate_resulting_frequency' do
-    context 'part 1: basic calculation' do
-      context 'when given [1, 1, 1]' do
-        let(:values) { [1, 1, 1] }
+    context 'when given [1, 1, 1]' do
+      let(:values) { [1, 1, 1] }
 
-        it 'produces 3' do
-          expect(chronal_calibrator.calculate_resulting_frequency).to eq 3
-        end
-      end
-
-      context 'when given [1, 1, -2]' do
-        let(:values) { [1, 1, -2] }
-
-        it 'produces 0' do
-          expect(chronal_calibrator.calculate_resulting_frequency).to eq 0
-        end
-      end
-
-      context 'when given [-1, -2, -3]' do
-        let(:values) { [-1, -2, -3] }
-
-        it 'produces -6' do
-          expect(chronal_calibrator.calculate_resulting_frequency).to eq -6
-        end
-      end
-
-      context 'when given the values in the text file' do
-        let(:values) { File.readlines('input_values.txt').map { |line| line.chomp.to_i } }
-
-        it 'produces 472' do
-          expect(chronal_calibrator.calculate_resulting_frequency).to eq 472
-        end
+      it 'produces 3' do
+        expect(subject.calculate_resulting_frequency).to eq 3
       end
     end
 
-    context 'part 2: frequency change counter' do
-      context 'when given [1, -2, 3, 1, ]'
+    context 'when given [1, 1, -2]' do
+      let(:values) { [1, 1, -2] }
+
+      it 'produces 0' do
+        expect(subject.calculate_resulting_frequency).to eq 0
+      end
+    end
+
+    context 'when given [-1, -2, -3]' do
+      let(:values) { [-1, -2, -3] }
+
+      it 'produces -6' do
+        expect(subject.calculate_resulting_frequency).to eq -6
+      end
+    end
+
+    context 'when given the values in the text file' do
+      let(:values) { file_values }
+
+      it 'produces 472' do
+        expect(subject.calculate_resulting_frequency).to eq 472
+      end
+    end
+  end
+
+  describe '#nth_recurring_frequency' do
+    context 'when given [1, -1]' do
+      let(:values) { [1, -1] }
+
+      it 'returns 1 as the first frequency reached twice' do
+        expect(subject.nth_recurring_frequency(n: 2)).to eq 0
+      end
+    end
+
+    context 'when given [3, 3, 4, -2, -4]' do
+      let(:values) { [3, 3, 4, -2, -4] }
+
+      it 'returns 10 as the first frequency reached twice' do
+        expect(subject.nth_recurring_frequency(n: 2)).to eq 10
+      end
+    end
+
+    context 'when given [1, -2, 3, 1]' do
+      let(:values) { [1, -2, 3, 1] }
+
+      it 'returns 2 as the first frequency reached twice' do
+        expect(subject.nth_recurring_frequency(n: 2)).to eq 2
+      end
+    end
+
+    context 'when given [-6, 3, 8, 5, -6]' do
+      let(:values) { [-6, 3, 8, 5, -6] }
+
+      it 'returns 5 as the first frequency reached twice' do
+        expect(subject.nth_recurring_frequency(n: 2)).to eq 5
+      end
+    end
+
+    context 'when given [7, 7, -2, -7, -4]' do
+      let(:values) { [7, 7, -2, -7, -4] }
+
+      it 'returns 14 as the first frequency reached twice' do
+        expect(subject.nth_recurring_frequency(n: 2)).to eq 14
+      end
+    end
+
+    context 'when given the file values' do
+      let(:values) { file_values }
+
+      it 'returns 66932 as the first frequency reached twice' do
+        expect(subject.nth_recurring_frequency(n: 2)).to eq 66932
+      end
     end
   end
 end
