14323082afbfbd891ef3d26483a895bf9c6661f2
Esther Leytush <esther.leytush@betterment.com>
2018-12-09 14:11:59 -0500

    start testing my version of levenshtein distance

--- day_2_inventory_management_system/inventory_manager.rb
+++ day_2_inventory_management_system/inventory_manager.rb
@@ -1,44 +1,88 @@
 class InventoryManager
-  attr_reader :ids
-  attr_accessor :lettercounted_values
+  attr_reader :words
+  attr_accessor :lettercounted_words, :letter_counts, :words_difference_groupings
 
-  def initialize(ids:)
-    @ids = ids
-    @lettercounted_values = []
+  def initialize(words:)
+    @words = words
+    @lettercounted_words = {}
   end
 
   def generate_checksum
-    lettercounted_values = generate_lettercounted_values(values: ids)
-    letter_counts = aggregate_letter_counts(values: lettercounted_values)
-    checksum(values: letter_counts)
+    @lettercounted_words = generate_lettercounted_words(words: words)
+    checksum_counts = aggregate_letter_counts(words: lettercounted_words)
+    checksum(words: checksum_counts)
+  end
+
+  def find_similar_words
+    @words_difference_groupings = generate_words_differences
+  end
+
+  def levenshtein_distance(first:, second:)
+    unless [first, second].all? { |word| word.is_a?(String) || word.is_a?(Hash) }
+      raise ArgumentError.new('Must be a string or a hash of letters to letter counts')
+    end
+    
+    first = count_letters(word: first) if first.is_a?(String)
+    second = count_letters(word: second) if second.is_a?(String)
+
+    # function levenshtein(a, b) {
+    #   var t = [], u, i, j, m = a.length, n = b.length;
+    #   if (!m) { return n; }
+    #   if (!n) { return m; }
+    #   for (j = 0; j <= n; j++) { t[j] = j; }
+    #   for (i = 1; i <= m; i++) {
+    #     for (u = [i], j = 1; j <= n; j++) {
+    #       u[j] = a[i - 1] === b[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+    #     } t = u;
+    #   } return u[n];
+    # }
+
+    binding.pry
   end
 
   private
 
-  def generate_lettercounted_values(values:)
-    values.map do |value|
-      count_letters(value: value).merge({ id: value })
-    end
+  def generate_lettercounted_words(words:)
+    words.map do |word|
+      counted = count_letters(word: word)
+      [
+        word,
+        {
+          letters: counted,
+          sorted: word.split('').sort.join(''),
+          2 => counted.values.include?(2),
+          3 => counted.values.include?(3)
+        }
+      ]
+    end.to_h
   end
 
-  def count_letters(value:)
-    counter = value.split('').group_by { |x| x }.values.map(&:length)
-    { 2 => counter.include?(2), 3 => counter.include?(3) }
+  def count_letters(word:)
+    counter = word.split('').group_by { |x| x }.values
+    counter.map { |letter_group| [ letter_group.first, letter_group.length ] }.to_h
   end
 
-  def aggregate_letter_counts(values:)
+  def aggregate_letter_counts(words:)
     two_counter = 0
     three_counter = 0
 
-    values.each do |value|
-      two_counter += 1 if value[2]
-      three_counter += 1 if value[3]
+    words.each do |key, word|
+      two_counter += 1 if word[2]
+      three_counter += 1 if word[3]
     end
 
     [two_counter, three_counter]
   end
 
-  def checksum(values:)
-    values.reduce(&:*)
+  def checksum(words:)
+    words.reduce(&:*)
+  end
+
+  def generate_words_differences
+    words.combination(2).to_a.map do |set|
+      letters_groups = set.map { |word| lettercounted_words[word][:letters] }
+      differences = []
+      { set: set, distance: levenshtein_distance(first: letters_groups.first, second: letters_groups.last) }
+    end
   end
 end
--- day_2_inventory_management_system/inventory_manager_spec.rb
+++ day_2_inventory_management_system/inventory_manager_spec.rb
@@ -3,19 +3,19 @@ require 'pry'
 require_relative 'inventory_manager.rb'
 
 RSpec.describe InventoryManager do
-  subject { InventoryManager.new(ids: ids) }
+  subject { InventoryManager.new(words: words) }
 
   describe '#generate_checksum' do
-    context "when ids are ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']" do
-      let(:ids) { ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab'] }
+    context "when words are ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']" do
+      let(:words) { ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab'] }
 
       it 'returns 12' do
         expect(subject.generate_checksum).to eq 12
       end
     end
 
-    context 'when ids are inputs from file' do
-      let(:ids) { File.readlines('ids_inputs.txt').map { |line| line.chomp } }
+    context 'when words are inputs from file' do
+      let(:words) { File.readlines('ids_inputs.txt').map { |line| line.chomp } }
 
       it 'returns 7872' do
         expect(subject.generate_checksum).to eq 7872
@@ -23,14 +23,34 @@ RSpec.describe InventoryManager do
     end
   end
 
-  describe '#find_similar_ids' do
+  describe '#find_similar_words' do
     before { subject.generate_checksum }
 
-    context "when ids are ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']" do
-      let(:ids) { ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz'] }
+    context "when words are ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']" do
+      let(:words) { ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz'] }
 
       it "returns 'fgij'" do
-        expect(subject.find_similar_ids).to eq 'fgij'
+        expect(subject.find_similar_words).to eq 'fgij'
+      end
+    end
+  end
+
+  describe '#levenshtein_distance' do
+    context 'when given bad inputs' do
+      let(:words) { [ 0, 0] }
+
+      it 'raises an ArgumentError' do
+        expect { subject.levenshtein_distance(first: 0, second: 0) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when words are 'abcde' and 'fghij'" do
+      let(:words) { [ 'abcde', 'fghij' ] }
+      let(:first) { words.first }
+      let(:second) { words.last }
+
+      it 'returns 5' do
+        expect(subject.levenshtein_distance(first: first, second: second)).to eq 5
       end
     end
   end
